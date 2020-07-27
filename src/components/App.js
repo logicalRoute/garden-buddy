d31e5abb0711ccb344fd2b38897fbbe05d1220c2
Paul <webdevpaulmason@gmail.com>
2020-07-27 14:11:51 -0700

    Issue 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,56 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
+import SearchBar from './SearchBar';
+import MainInfo from './MainInfo';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { data: [], searchTerm: '', dataPass: {}};
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  
+  componentDidMount = () => {
+    axios.get('http://harvesthelper.herokuapp.com/api/v1/plants?api_key=4bd977e41e7468b5633f1111f82d50be')
+    .then((response) => {
+      this.setState({ data: response.data})
+      console.log(this.state.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  onSubmit = (term) => {
+    console.log('term:', term);
+    let dataToPass = this.search(term, this.state.data);
+    console.log('dataToPass: ', dataToPass);
+    this.setState({ searchTerm: term, dataPass: dataToPass});
+    console.log(this.state.dataToPass);
+    //Grab the data based upon search term
+  }
+
+  search = (nameKey, array) => {
+    for(let i = 0; i < array.length; i++){
+      if(array[i].name.toLowerCase() === nameKey) {
+        return array[i];
+      }
+    }
+  }
+
   render() {
-    return <h1>TEST</h1>
+    return (
+    <div>
+      <SearchBar 
+        onSubmit={this.onSubmit}
+      />
+      <MainInfo 
+        data={this.dataPass}
+      />
+      {this.state.searchTerm}
+    </div>
+    );
   }
 }
 export default App;
---
+++ src/components/MainInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class MainInfo extends Component {
+  render(){
+    return(
+      <h1>THIS IS THE MAININFO WINDOW</h1>
+    );
+
+  }
+}
+export default MainInfo;
